7c31da9978611c79ad0c52ded3934f381a14dfa4
Александр Фомин <neiasit@bk.ru>
2019-12-15 18:50:26 +1000

    1

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
  * @param {String} date
  * @returns {Object}
  */
-module.exports = function (date) {
+module.exports = function(date) {
+    var _date = new Date(date);
+    var DateCalc = {
+        date: _date,
 
+        add: function(count, scale) {
+
+            return this;
+        },
+        subtract: function(count, mesurment) {
+
+            return this;
+        },
+        get value() {
+
+            return `${year}-${month}-${day} ${hour}:${minute}`;
+        }
+    }
 };
