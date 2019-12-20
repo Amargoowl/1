b569f16a2689c347dcfd90165226334255a57cbb
Александр Фомин <neiasit@bk.ru>
2019-12-20 20:12:50 +1000

    2

--- index.js
+++ index.js
@@ -4,19 +4,56 @@
  */
 module.exports = function(date) {
     var _date = new Date(date);
-    console.log(_date.toISOString());
-    console.log(_date.toDateString());
+    _date.setMinutes(_date.getMinutes() + Math.abs(_date.getTimezoneOffset()));
+    var SCALE = ['years', 'months', 'days', 'hours', 'minutes']
     return {
         add: function(count, scale) {
-
+            if (count < 0 || !SCALE.includes(scale)) {
+                throw new TypeError();
+            }
+            switch (scale) {
+                case 'years':
+                    _date.setFullYear(_date.getFullYear() + count);
+                    break;
+                case 'months':
+                    _date.setMonth(_date.getMonth() + count);
+                    break;
+                case 'days':
+                    _date.setDate(_date.getDate() + count);
+                    break;
+                case 'hours':
+                    _date.setHours(_date.getHours() + count);
+                    break;
+                case 'minutes':
+                    _date.setMinutes(_date.getMinutes() + count);
+                    break;
+            };
             return this;
         },
         subtract: function(count, scale) {
-
+            if (count < 0 || !SCALE.includes(scale)) {
+                throw new TypeError();
+            }
+            switch (scale) {
+                case 'years':
+                    _date.setFullYear(_date.getFullYear() - count);
+                    break;
+                case 'months':
+                    _date.setMonth(_date.getMonth() - count);
+                    break;
+                case 'days':
+                    _date.setDate(_date.getDate() - count);
+                    break;
+                case 'hours':
+                    _date.setHours(_date.getHours() - count);
+                    break;
+                case 'minutes':
+                    _date.setMinutes(_date.getMinutes() - count);
+                    break;
+            };
             return this;
         },
         get value() {
-
             return _date.toISOString().slice(0, 16).replace('T', ' ');
         }
     }
